dc8500b657dd44ca3fcabb00aa35ff15463a83d2
Angela Gola <angelagola.ko@gmail.com>
2022-05-05 22:09:36 -0400

    Working on changing colors of tasks. Updated CSS/HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,35 @@
-// Saves to page
-//Stored and doesnt delete when refreshing.
-// use moment.js or another library.
+
+// Displays time
+let nowMoment = moment().format("MMMM Do YYYY");
+let displayDate = document.getElementById("currentDay");
+displayDate.innerHTML = nowMoment;
+let currentHour = moment().format("HH");
+//Button to clear local storage and clear all contents
+$("#clearFieldsBtn").click(function (event) {
+    $('textarea').val("");
+    localStorage.clear();
+
+$(".timeTo").each(function() {
+    var timeIs = $(this).attr("id").split("-"[1]);
+    
+    if (currentHour == timeIs) {
+        $(this).addClass("current");
+        
+    }
+})
+});
+//Saves to local storage
+if(localStorage.text != undefined) {
+    localStorage.text.split(',').forEach(function (text,i) {
+        $('textarea').eq(i).val(text);
+    });
+}
+//Saves to local storage
+$(".saveBtn").click(function (event) {
+    let store = [];
+    $.each($('textarea'), function (i,text) {
+        store.push($(text).val());
+    });
+
+    localStorage.text = store.join(',');
+})
