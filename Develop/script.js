36f36f2995e441e02a95ad7c81994b5f0e70697b
Angela Gola <angelagola.ko@gmail.com>
2022-05-07 13:23:20 -0400

    Added colors to mark as past,present,future and some stylin

--- Develop/script.js
+++ Develop/script.js
@@ -4,20 +4,30 @@ let nowMoment = moment().format("MMMM Do YYYY");
 let displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = nowMoment;
 let currentHour = moment().format("HH");
+
+//alert("Connected")
+
 //Button to clear local storage and clear all contents
 $("#clearFieldsBtn").click(function (event) {
     $('textarea').val("");
     localStorage.clear();
+}) // close this off
 
-$(".timeTo").each(function() {
-    var timeIs = $(this).attr("id").split("-"[1]);
-    
+$(".timeTo").each(function(index) {
+console.log(currentHour);
+    var timeIs = $(this).attr("id").split("-")[1];
+    console.log(timeIs)
     if (currentHour == timeIs) {
-        $(this).addClass("current");
-        
+        $(this).addClass("present");
+    } else if (currentHour < timeIs) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    } else if (currentHour > timeIs) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
     }
 })
-});
+
 //Saves to local storage
 if(localStorage.text != undefined) {
     localStorage.text.split(',').forEach(function (text,i) {
